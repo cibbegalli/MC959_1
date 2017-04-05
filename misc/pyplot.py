051026e72fec280f376c20ad2a20e8348c210e3b
Joao Dalben <jhdalben@gmail.com>
2017-04-05 18:18:41 -0300

    Adicionado o plot da trajetória do robô.

--- misc/pyplot.py
+++ misc/pyplot.py
@@ -4,12 +4,23 @@ x = []
 y = []
 
 with open("../files/gridmapbylaser.txt") as f:
-#with open("teste.txt") as f:
     coords = csv.reader(f, delimiter='\t', quotechar='|')
     for line in coords:
-        x.append(float(line[0]))
-        y.append(float(line[1]))
-
+        if line:
+            x.append(float(line[0]))
+            y.append(float(line[1]))
 plt.plot(x, y, 'r.')
+
+x = []
+y = []
+
+with open("../files/graph.csv") as f:
+    coords = csv.reader(f, delimiter='\t', quotechar='|')
+    for line in coords:
+        if line:
+            x.append(float(line[1]))
+            y.append(float(line[2]))
+plt.plot(x, y, 'b.')
+
 plt.axis()
 plt.show()
